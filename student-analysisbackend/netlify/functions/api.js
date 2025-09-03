5a6a1fca0df8da06ad103a36b090d17a0916c558
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-09-03 14:01:15 +0530

    add

--- student-analysisbackend/netlify/functions/api.js
+++ student-analysisbackend/netlify/functions/api.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../../server'); // Path to server.js
+const app = require('../../server'); // Points to server.js in root
 
 module.exports.handler = serverless(app);
